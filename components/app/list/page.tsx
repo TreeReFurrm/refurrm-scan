41e79198d439ee44f8ee2beeca97d7e9dda7315c
Reese <harleygirley97@gmail.com>
2025-12-16 00:30:04 -0600

    chore: remove duplicate component files (keep newest/src variants)

--- components/app/list/page.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-import { InstantListingGenerator } from '@/components/valuscan/instant-listing-generator';
-
-export default function ListPage() {
-  return <InstantListingGenerator />;
-}
